c67e994365565e4ed4224d50f1b32c08d1f56a78
2Smean <136636643+2Smean@users.noreply.github.com>
2025-01-22 19:28:22 +0900

    [Feature] 회원 도메인 실행과 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -5,7 +5,7 @@ package hello.core.member;
  */
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // DIP 위반
 
     @Override
     public void join(Member member) {
